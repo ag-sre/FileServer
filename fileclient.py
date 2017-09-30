de62bcc1215c581871c8e95f9532fb91a953617c
Ankita <ankitagandhi.6694@gmail.com>
2017-09-30 02:52:23 -0500

    Looping done, directeries removed

--- fileclient.py
+++ fileclient.py
@@ -14,7 +14,7 @@ def Download(s):
             message=raw_input("File Exists,"+str(filesize)+"bytes (y/n)?\n")
             if message =='y':
                 s.send('OK')
-                f=open('client/new_'+filename,'wb')
+                f=open(filename,'wb')
                 data=s.recv(1024)
                 totalRecv=len(data)
                 f.write(data)
@@ -26,7 +26,7 @@ def Download(s):
                 print "Download Complete!"
         else:
             print "File does not exist"
-    s.close()
+
 
 def Rename(s):
     s.send("03")
@@ -57,63 +57,67 @@ def Upload(s):
     s.send("02")
     ack = s.recv(1024)
     if (ack.decode('utf-8') == "OK"):
+        fl = []
+        for dirpath, dirnames, filenames in os.walk('./'):
+            fl.extend(filenames)
+        print (fl)
         file = raw_input("enter file to upload?(q to quit) ->\n")
         if file != 'q':
             file = file.split('\n')
-            fl = []
-            for dirpath, dirnames, filenames in os.walk('./client'):
-                fl.extend(filenames)
-            print (file[0])
             if file[0] in fl:
-                print("File present in system")
+                print("File present in system,uploading..")
                 s.sendall(file[0].encode('utf-8'))
                 if(s.recv(1024) == "ERR"):
                     print("File already exists in Server!!")
                 else:
-                    with open('client/'+file[0], 'rb') as file_to_send:
+                    with open(file[0], 'rb') as file_to_send:
                         for data in file_to_send:
                             s.sendall(data)
                     print('end')
                     print("Upload Complete!")
+            else:
+                print "file does not exist on client"
     else:
         print ("Connection Error!")
+
 
 def Delete(s):
     s.send("04")
     ack = s.recv(1024)
     if (ack.decode('utf-8') == "OK"):
-        file = raw_input("enter file to delete?(q to quit) ->\n")
-        if file != 'q':
-            file = file.split('\n')
-            fl = []
-            for dirpath, dirnames, filenames in os.walk('./server'):
-                fl.extend(filenames)
-            print (file[0])
-            if file[0] in fl:
-                print("File present in system")
-                s.sendall(file[0].encode('utf-8'))
-                if (s.recv(1024) == "ERR"):
-                    print("File doesnt exists in Server!!")
-                else:
-                    print('end')
-                    print("Delete Complete!")
+        files_in_server=s.recv(4096)
+        files_to_print=pickle.loads(files_in_server)
+        print (files_to_print)
+        file_to_delete = raw_input("enter file to delete?(q to quit) ->\n")
+        if file_to_delete != 'q':
+            file_to_delete = file_to_delete.split('\n')
+            s.sendall(file_to_delete[0].encode('utf-8'))
+            if (s.recv(1024) == "ERR"):
+                print("File doesnt exists in Server!!")
+            else:
+                print('end')
+                print("Delete Complete!")
     else:
         print ("Connection Error!")
-    s.close()
 
 def Main():
     host='127.0.0.1'
     port=5000
-    s=socket.socket()
-    s.connect((host,port))
-    message = raw_input("Select your option:\n1. Download\n2. Upload\n3. Rename\n4.Delete)?")
-    if message=='1':
-        Download(s)
-    elif message=='2':
-        Upload(s)
-    elif message=='3':
-        Rename(s)
-    elif message=='4':
-        Delete(s)
+    message = '1'
+    s = socket.socket()
+    s.connect((host, port))
+    while message != '5':
+        message = raw_input("Select your option:\n1. Download\n2. Upload\n3. Rename\n4. Delete\n5. Quit\n")
+        if message=='1':
+            Download(s)
+        elif message=='2':
+            Upload(s)
+        elif message=='3':
+            Rename(s)
+        elif message=='4':
+            Delete(s)
+    if message=='5':
+        s.send("05")
+        s.close()
 if __name__=='__main__':
     Main()
--- fileserver_singlethread.py
+++ fileserver_singlethread.py
@@ -3,71 +3,80 @@ import threading
 import os
 
 def retriveFile(name, sok):
-    useresponse = sok.recv(1024)
-    print (useresponse[:2])
-    fl = []
-    for dirpath, dirnames, filenames in os.walk('./server'):
-        fl.extend(filenames)
-    bytesToSend = pickle.dumps(fl)
-    print "test"
-    if useresponse[:2] == "01":
-        sok.send(bytesToSend)
-        filename = sok.recv(1024)
-        if os.path.isfile('server/'+filename):
-            sok.send("Exists " + str(os.path.getsize('server/'+filename)))
-            useresponse = sok.recv(1024)
-            if useresponse[:2] == "OK":
-                with open('server/'+filename, 'rb') as f:
-                    bytesToSend = f.read(1024)
-                    sok.send(bytesToSend)
-                    while bytesToSend != "":
+    useresponse="01"
+    while useresponse!="05":
+        print "testloop"
+        useresponse = sok.recv(1024)
+        print (useresponse[:2])
+        fl = []
+        for dirpath, dirnames, filenames in os.walk('./'):
+            fl.extend(filenames)
+        bytesToSend = pickle.dumps(fl)
+        print "test"
+        if useresponse[:2] == "01":
+            sok.send(bytesToSend)
+            filename = sok.recv(1024)
+            if os.path.isfile(filename):
+                sok.send("Exists " + str(os.path.getsize(filename)))
+                useresponse = sok.recv(1024)
+                if useresponse[:2] == "OK":
+                    with open(filename, 'rb') as f:
                         bytesToSend = f.read(1024)
                         sok.send(bytesToSend)
-        else:
-            sok.send("ERR")
-    elif useresponse[:2] == "02":
-        sok.sendall("OK".encode('utf-8'))
-        filename = sok.recv(1024).decode('utf-8')
-        print(filename)
-        uploadfile = "upload_" + filename
-        if os.path.isfile('server/' + uploadfile):
-            print("File already exists in Server")
-            sok.send("ERR")
-        else:
-            with open('server/'+uploadfile, 'wb') as file_to_write:
-                while True:
-                    data = sok.recv(1024)
-                    print(data)
-                    if not data:
-                        break
-                    file_to_write.write(data)
-                print("File Uploaded")
-    elif useresponse[:2] == "03":
-        print "test2"
-        sok.send(bytesToSend)
-        filename = sok.recv(1024)
-        if os.path.isfile('server/' + filename):
-            sok.send("Exists " + str(os.path.getsize('server/' + filename)))
-            newname = sok.recv(1024)
-            os.rename('server/'+filename,'server/'+newname)
-            sok.send("Success")
-            newf=[]
-            for dirpath, dirnames, nfilenames in os.walk('./server'):
-                newf.extend(nfilenames)
-            newFileList = pickle.dumps(newf)
-            sok.send(newFileList)
-        else:
-            sok.send("ERR")
-    elif useresponse[:2] == "04":
-        sok.sendall("OK".encode('utf-8'))
-        filename = sok.recv(1024)
-        print(filename)
-        if os.path.isfile('server/' + filename):
-            os.remove('server/' +filename)
-            print("File removed successfully!")
-        else:
-            print("File not found in Server!")
-            sok.send("ERR")
+                        while bytesToSend != "":
+                            bytesToSend = f.read(1024)
+                            sok.send(bytesToSend)
+            else:
+                sok.send("ERR")
+        elif useresponse[:2] == "02":
+            sok.sendall("OK".encode('utf-8'))
+            filename = sok.recv(1024).decode('utf-8')
+            print(filename)
+            uploadfile = filename
+            if os.path.isfile(uploadfile):
+                print("File already exists in Server")
+                sok.send("ERR")
+            else:
+                with open(uploadfile, 'wb') as file_to_write:
+                    while True:
+                        data = sok.recv(1024)
+                        print(data)
+                        if not data:
+                            break
+                        file_to_write.write(data)
+                    print("File Uploaded")
+        elif useresponse[:2] == "03":
+            print "test2"
+            sok.send(bytesToSend)
+            filename = sok.recv(1024)
+            if os.path.isfile(filename):
+                sok.send("Exists " + str(os.path.getsize(filename)))
+                newname = sok.recv(1024)
+                os.rename(filename,newname)
+                sok.send("Success")
+                newf=[]
+                for dirpath, dirnames, nfilenames in os.walk('./'):
+                    newf.extend(nfilenames)
+                newFileList = pickle.dumps(newf)
+                sok.send(newFileList)
+            else:
+                sok.send("ERR")
+        elif useresponse[:2] == "04":
+            sok.sendall("OK".encode('utf-8'))
+            fl = []
+            for dirpath, dirnames, filenames in os.walk('./'):
+                fl.extend(filenames)
+            filesToSend=pickle.dumps(fl)
+            sok.send(filesToSend)
+            filename = sok.recv(1024)
+            print(filename)
+            if os.path.isfile(filename):
+                os.remove(filename)
+                print("File removed successfully!")
+                sok.send("Deleted")
+            else:
+                print("File not found in Server!")
+                sok.send("ERR")
     sok.close()
 
 def Main():
@@ -77,11 +86,11 @@ def Main():
     s.bind((host, port))
     s.listen(5)
     print "Server started"
+
     c, addr = s.accept()
     print "client connected to ip:<", str(addr), ">"
     t = threading.Thread(target=retriveFile, args=("retrThread", c))
     t.start()
-    s.close()
 
 
 if __name__ == '__main__':
